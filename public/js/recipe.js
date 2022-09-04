00dd599a4911fda8ba795e26e91e441aabb70368
ahmedDevs <ahmed.soft.dev@gmail.com>
2022-09-04 02:42:58 -0400

    i

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,22 +1,18 @@
 const deleteBtn = document.querySelectorAll('.del')
 // const recipeItem = document.querySelectorAll('span.not')
 const recipeSpan = document.querySelectorAll('.recipeTitle')
-
+const recipeForm = document.querySelector('.recipeForm')
 
 
 Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+    el.addEventListener('click', deleteRecipe)
 })
 
-// Array.from(recipeSpan).forEach((el)=>{
-//     el.addEventListener('click', openThread)
-// })
 
-// Array.from(todoComplete).forEach((el)=>{
-//     el.addEventListener('click', markIncomplete)
-// })
 
-async function deleteTodo(){
+
+
+async function deleteRecipe(){
     const todoId = this.parentNode.dataset.id
     console.log(todoId)
     try{
@@ -38,53 +34,5 @@ async function deleteTodo(){
 
 
 
-// async function openThread() {
-//     // const threadTitle = document.querySelectorAll('.recipeTitle')
 
-//     // const comment = document.getElementById('postToRender').textContent
 
-//     // const commentContent = document.querySelector('.postRender').textContent
-//     const commentContent = this.parentNode.parentNode.childNodes[1].innerText
-
-//     console.log(commentContent)
-
-//     threadTitle.appendChild(threadTitle.innerText)
-//     localStorage.setItem('thread',commentContent)
-    
-// }
-
-// async function markComplete(){
-//     const todoId = this.parentNode.dataset.id
-//     try{
-//         const response = await fetch('todos/markComplete', {
-//             method: 'put',
-//             headers: {'Content-type': 'application/json'},
-//             body: JSON.stringify({
-//                 'todoIdFromJSFile': todoId
-//             })
-//         })
-//         const data = await response.json()
-//         console.log(data)
-//         location.reload()
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
-
-// async function markIncomplete(){
-//     const todoId = this.parentNode.dataset.id
-//     try{
-//         const response = await fetch('todos/markIncomplete', {
-//             method: 'put',
-//             headers: {'Content-type': 'application/json'},
-//             body: JSON.stringify({
-//                 'todoIdFromJSFile': todoId
-//             })
-//         })
-//         const data = await response.json()
-//         console.log(data)
-//         location.reload()
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
