336809f57476f7ec61d214cd2955acb4f44b2610
najwanan <theunnamedrd@gmail.com>
2022-09-05 15:56:25 -0700

    adding cors into server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
@@ -28,6 +29,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(cors)
 app.use(logger('dev'))
 // Sessions
 app.use(
