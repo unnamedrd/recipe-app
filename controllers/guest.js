85fd159f9000eace35aa3512ac716dcef4262b40
ahmedDevs <ahmed.soft.dev@gmail.com>
2022-09-05 13:26:35 -0400

    Add matching style to guest recipes page and local storage

--- controllers/guest.js
+++ controllers/guest.js
@@ -1,10 +1,15 @@
 
 
+
+
+
 module.exports = {
     getRecipes: (req,res) => {
         res.render('guest-recipe.ejs')
     },
     getIngredients: (req,res) => {
         res.redner('guest-ingredient.ejs')
-    }
+    },
+  
+    
 }
