6e5947bab959013d90d5995748e0e794593ae4e3
LHuynh05 <lahuynh@hawaii.edu>
2022-09-06 11:41:06 -1000

    fixed conflicts

--- routes/recipe.js
+++ routes/recipe.js
@@ -8,12 +8,11 @@ router.get('/', ensureAuth, recipesController.getRecipes)
 
 router.post('/createRecipe', recipesController.createRecipe)
 
-<<<<<<< HEAD
+
 // created new route for sorted recipe list---havent tested.
 router.get('/getSortedRecipe', recipesController.getSortedRecipe)
 
-=======
->>>>>>> 6e17cb8857563f1d226ae47f659bb01e206c9604
+
 router.delete('/deleteRecipe', recipesController.deleteRecipe)
 
 //add get statements and route to Controller for image
