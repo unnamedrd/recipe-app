0508006456e4013256226a12499cd08f790c0982
LHuynh05 <lahuynh@hawaii.edu>
2022-09-15 23:25:28 -1000

    updated sort function for sorting recipe

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
@@ -11,30 +10,11 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const ingredientRoutes = require('./routes/ingredient')
 const recipeRoutes = require('./routes/recipe')
-<<<<<<< HEAD
 const multer = require('multer') //multer allows uploading  files for the img
-=======
-const guestRoutes = require('./routes/guest')
-=======
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const flash = require("express-flash");
-const logger = require("morgan");
-const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const ingredientRoutes = require("./routes/ingredient");
-const recipeRoutes = require("./routes/recipe");
-const guestRoutes = require("./routes/guest");
->>>>>>> 6e17cb8857563f1d226ae47f659bb01e206c9604
+const guestRoutes = require('./routes/guest');
 
->>>>>>> 609104ff7e9dfb02c6aeccc2ed615889abc64173
+
+
 
 require("dotenv").config({ path: "./config/.env" });
 
@@ -49,9 +29,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(logger("dev"));
+
+
 // Sessions
 app.use(
-<<<<<<< HEAD
+
     session({
       secret: 'keyboard cat',
       resave: false,
@@ -62,17 +44,17 @@ app.use(
  
 // multer middleware
 app.use(multer({dest:'./routes/recipe'}).single()) //adding destination for file uploading not sure if this will work, but leaving it here for now
-=======
+
   session({
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
-);
+;
 
 //bodyParser middleware
->>>>>>> 6e17cb8857563f1d226ae47f659bb01e206c9604
+
 
 // Passport middleware
 app.use(passport.initialize());
